6de61bbb500b6b13125518af0f9aad5ab03e1d48
Robert Rossmiller <robertrossmiller@Robs-Macbook-Pro.local>
2018-03-21 13:20:19 -0500

    edited setup() and loop() formatting

--- DrinkMixer.ino
+++ DrinkMixer.ino
@@ -2,12 +2,12 @@
  * 
  */
  
- void setup() {
-  // put your setup code here, to run once:
+ void setup() 
+{
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
 
 }
