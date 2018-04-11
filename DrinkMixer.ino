f8b1b60373a066d6883b6952324540eb6bf89d30
Robert Rossmiller <robertrossmiller@Robs-Macbook-Pro.local>
2018-04-11 14:00:37 -0500

    Code up to starting to add/read fluid flow

--- DrinkMixer.ino
+++ DrinkMixer.ino
@@ -29,33 +29,66 @@
 // Init LiquidCrystal object with pins corresponding to Crystal Ball project
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-
 // Valves
 int alcValvePin; // attach the alcohol valve to a digital pin
 int mixValvePin; // attach the mixer valveto a digital pin
 
-
 // Potentiometer
 int const potPin = A0; // analog pin used to connect the potentiometer
 int potVal;  // variable to read the value from the analog pin
-int angle;   // variable to hold the angle for the servo motor
+
+// Flow meters
+int 
+
+// Other pins
+int confirmButtonState = 0;
+int confirmButtonPin;
+
+int drinkStrength;
 
  void setup() 
 {
     Serial.begin(9600); // open a serial connection to your computer
     lcd.begin(16, 2); //begin reading/writing to LCD monitor
+    pinMode(confirmButtonPin, INPUT);
+    pinMode(killButtonPin, INPUT);
 }
 
 void loop() 
 {
-  potVal = analogRead(potPin); // read the value of the potentiometer
-  // print out the value to the Serial Monitor
-  Serial.print("potVal: ");
-  Serial.println(potVal);
-  delay(100);
+  // Welcome user
+    lcd.clear();
+    lcd.println("Place cup under spout");
+    lcd.print("Press Confirm to continue");
+    
+    while(confirmButtonState == 0)
+    {
+      confirmButtonState = digitalRead(confirmButtonPin);
+    }
+
+    confirmButtonState = 0;
+    while(confirmButtonState == 0)
+    {
+      lcd.clear();
+      lcd.print("Current Strength: ");
+      lcd.println(drinkStrength());
+      lcd.print("Push Confirm to continue");
+      confirmButtonState = digitalRead(confirmButtonPin);
+    }
+
+    confirmButtonState = 0;
+    // Start mixing drink
+    lcd.clear();
+    lcd.print("Mixing...");
+    
   
 }
 
 
+int drinkStrenght()
+{
+  drinkStrength = map(potVal, 0, 1023, 0, 10);
+  return drinkStrength;
+}
 
 
